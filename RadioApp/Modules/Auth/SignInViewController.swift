893757f9c6e6a3fa97c1f9aafddda32b53d31761
Ivan Semikin <i.semikin.94@icloud.com>
2024-07-29 22:03:34 +0200

    Created PreviewProvider

--- RadioApp/Modules/Auth/SignInViewController.swift
+++ RadioApp/Modules/Auth/SignInViewController.swift
@@ -6,17 +6,18 @@
 //
 
 import UIKit
+import SwiftUI
 
 final class SignInViewController: UIViewController {
     override func viewDidLoad() {
-        
+        setupUI()
     }
 }
 
 // MARK: - Private Methods
 private extension SignInViewController {
     func setupUI() {
-        
+        view.backgroundColor = .black
     }
     
     func addSubviews() {
@@ -25,5 +26,23 @@ private extension SignInViewController {
     
     func setConstraints() {
         
+    }
+}
+
+struct SignInViewControllerPreview: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> SignInViewController {
+        return SignInViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: SignInViewController, context: Context) {
+        // Code to update the view controller if needed
+    }
+}
+
+struct SignInViewControllerPreview_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInViewControllerPreview()
+            .edgesIgnoringSafeArea(.all)
     }
 }
