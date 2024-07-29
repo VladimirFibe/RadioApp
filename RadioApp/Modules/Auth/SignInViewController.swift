ff86a7328c6c5998f1f70359c86733f825daa359
Ivan Semikin <i.semikin.94@icloud.com>
2024-07-29 22:00:19 +0200

    Create SignInViewController with new directory

---
+++ RadioApp/Modules/Auth/SignInViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SignInViewController.swift
+//  RadioApp
+//
+//  Created by Иван Семикин on 29/07/2024.
+//
+
+import UIKit
+
+final class SignInViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
