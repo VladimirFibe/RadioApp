940207e5400a2bce3f51128bd1fc39be970ba988
Ivan Semikin <i.semikin.94@icloud.com>
2024-07-31 17:35:11 +0200

    Created ConnectWithGoogleView

---
+++ RadioApp/Modules/Auth/Views/ConnectWithGoogleView.swift
@@ -0,0 +1,23 @@
+//
+//  ConnectWithGoogleView.swift
+//  RadioApp
+//
+//  Created by Иван Семикин on 31/07/2024.
+//
+
+import UIKit
+
+final class ConnectWithGoogleView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview() {
+    ConnectWithGoogleView()
+}
