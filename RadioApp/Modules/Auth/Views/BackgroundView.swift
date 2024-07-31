bf9ec4128cf2f5a769f234fefb80c3a3dc4e4e52
Ivan Semikin <i.semikin.94@icloud.com>
2024-07-31 16:40:24 +0200

    Created BackgroundView with backgroundPart1ImageView

---
+++ RadioApp/Modules/Auth/Views/BackgroundView.swift
@@ -0,0 +1,25 @@
+//
+//  BackgroundView.swift
+//  RadioApp
+//
+//  Created by Иван Семикин on 31/07/2024.
+//
+
+import UIKit
+
+final class BackgroundView: UIView {
+    
+    private let backgroundPart1ImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(named: "backgroundAppPart1")
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
