480c1ca6292f232b65b9fa39ffd7f31f5fa86b90
Elena Loginova <iosdevlogin@gmail.com>
2024-08-09 01:05:39 +0500

    Add AllStations

---
+++ RadioApp/Modules/AllStations/AllStationsCell.swift
@@ -0,0 +1,12 @@
+//
+//  AllStationsCell.swift
+//  RadioApp
+//
+//  Created by Елена Логинова on 09.08.2024.
+//
+
+import UIKit
+
+class AllStationsCell: UICollectionViewCell {
+    
+}
---
+++ RadioApp/Modules/AllStations/AllStationsView.swift
@@ -0,0 +1,20 @@
+//
+//  AllStationsView.swift
+//  RadioApp
+//
+//  Created by Елена Логинова on 09.08.2024.
+//
+
+import UIKit
+
+class AllStationsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
