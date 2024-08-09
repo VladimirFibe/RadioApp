c8c91cb597a68267812ae13add9118792b813803
Elena Loginova <iosdevlogin@gmail.com>
2024-08-09 18:48:04 +0500

    Add SearchVC

---
+++ RadioApp/Modules/Search/SearchCell.swift
@@ -0,0 +1,12 @@
+//
+//  SearchCell.swift
+//  RadioApp
+//
+//  Created by Елена Логинова on 09.08.2024.
+//
+
+import UIKit
+
+class SearchCell: UICollectionViewCell {
+    
+}
---
+++ RadioApp/Modules/Search/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  RadioApp
+//
+//  Created by Елена Логинова on 09.08.2024.
+//
+
+import UIKit
+
+class SearchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
